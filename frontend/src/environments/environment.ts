d72fa3f19603da055e63939a9104bae3cbe0fbe3
eugenlee <eugenelukelee@gmail.com>
2020-04-26 02:04:27 -0700

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'eugenelee.auth0.com', // the auth0 domain prefix
+    url: 'eugenelee', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'nk2NxAYYPpPE4mvPUm3ENZMzczANpmLA', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
